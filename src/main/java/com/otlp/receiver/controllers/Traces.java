7087a63e26b51a55a941ea27a540e75f5f83f8d5
sajjad <sssanikhani@gmail.com>
2023-06-16 15:15:35 +0330

    fix: read data from JsonProtobuf request input stream for traces

--- src/main/java/com/otlp/receiver/controllers/Traces.java
+++ src/main/java/com/otlp/receiver/controllers/Traces.java
@@ -7,35 +7,42 @@ import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.commons.codec.DecoderException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 
 @RestController
 @RequestMapping("/v1/traces")
 public class Traces {
-    @PostMapping
-    public Object addTrace(@RequestBody HashMap<String, Object> requestBody, HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
-        // Replacing SpanID and TraceID Hex Values with Base64
-        String normalizedBody;
-        try {
-            normalizedBody = DataNormalizer.normalizeRequestBody(requestBody);
-        } catch (DecoderException e) {
-            throw new RuntimeException(e);
-        }
+    @PostMapping(consumes = "application/x-protobuf")
+    public Object addTrace(@RequestBody HashMap<String, Object> requestBody, HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        // Replacing SpanID and TraceID Hex Values with Base64
+//        String normalizedBody;
+//        try {
+//            normalizedBody = DataNormalizer.normalizeRequestBody(requestBody);
+//        } catch (DecoderException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        // Converting Body to Protobuf Messages
+//        ExportTraceServiceRequest.Builder tracesRequestBuilder = ExportTraceServiceRequest.newBuilder();
+//        try {
+//            JsonFormat.parser().merge(normalizedBody, tracesRequestBuilder);
+//        } catch (InvalidProtocolBufferException e) {
+//            throw new RuntimeException(e);
+//        }
 
-        // Converting Body to Protobuf Messages
-        ExportTraceServiceRequest.Builder tracesRequestBuilder = ExportTraceServiceRequest.newBuilder();
-        try {
-            JsonFormat.parser().merge(normalizedBody, tracesRequestBuilder);
-        } catch (InvalidProtocolBufferException e) {
-            throw new RuntimeException(e);
-        }
-        ExportTraceServiceRequest requestMessage = tracesRequestBuilder.build();
+
+
+
+        ExportTraceServiceRequest requestMessage = ExportTraceServiceRequest.parseFrom(request.getInputStream());
+        System.out.println(requestMessage.getResourceSpansList().get(0).getResource().getAttributesList());
 
 
 //        System.out.println(requestMessage.getResourceSpansCount());
